b8b8a69e6dc630602fc52047332556a26a471749
Raysa <lutux17@gmail.com>
2019-02-01 13:37:03 -0500

    firstProject

---
+++ Testing/src/main/java/Raysa/Testing/App.java
@@ -0,0 +1,13 @@
+package Raysa.Testing;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
